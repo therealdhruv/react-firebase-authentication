b230711910657da1a5bd2ff684f6518fd5815dbe
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-05-18 02:52:39 +0530

    feat: basic routing added

---
+++ src/components/App/index.js
@@ -0,0 +1,45 @@
+// The App component is the perfect place to render the Navigation component because it always renders the Navigation component but replaces other components (pages) based on the routes. Basically, the App component is the container into which all our fixed components (e.g. navigation bar, sidebar, footer), as well as the components that are displayed depending on the route in the URL (e.g. account page, login page, password forget page), will go.
+
+
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import LandingPage from '../Landing';
+import SignUpPage from '../SignUp';
+import SignInPage from '../SignIn';
+import PasswordForgetPage from '../PasswordForget';
+import HomePage from '../Home';
+import AccountPage from '../Account';
+import AdminPage from '../Admin';
+
+import * as ROUTES from '../../constants/routes';
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr />
+      <Routes>
+        <Route exact path = {ROUTES.LANDING} component = {LandingPage} />
+        <Route path = {ROUTES.SIGN_UP} component = {SignUpPage} />
+        <Route path = {ROUTES.SIGN_IN} component = {SignInPage} />
+        <Route path = {ROUTES.PASSWORD_FORGET} component = {PasswordForgetPage} />
+        <Route path = {ROUTES.HOME} component = {HomePage} />
+        <Route path = {ROUTES.ACCOUNT} component = {AccountPage} />
+        <Route path = {ROUTES.ADMIN} component = {AdminPage} />
+      </Routes>
+    </div>
+  </Router>
+);
+
+export default App;
+
+
+
+
---
+++ src/constants/routes.js
@@ -0,0 +1,9 @@
+// This is the file where we can centralize all the routes of our application
+
+export const LANDING = '/';
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const ADMIN = '/admin';
+export const PASSWORD_FORGET = '/pw-forget';
