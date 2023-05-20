b5968bca757057f77094214051147a4a71b42635
Dhruv Pankaj Patel <dhruvkochi@gmail.com>
2023-05-21 01:23:54 +0530

    fix: Update src/index.js for React 18 API changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import './index.css';
+// import * as serviceWorker from './serviceWorker';
+
+// import App from './components/App';
+// import Firebase from './components/Firebase';
+
+// import FirebaseContext from './components/Firebase/context';
+
+
+
+// ReactDOM.render(
+//   <FirebaseContext.Provider value = { new Firebase() }>
+//     <App />
+//   </FirebaseContext.Provider>,
+//   document.getElementById('root'),
+// );
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: http://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
@@ -9,16 +35,10 @@ import Firebase from './components/Firebase';
 
 import FirebaseContext from './components/Firebase/context';
 
-
-
-ReactDOM.render(
-  <FirebaseContext.Provider value = { new Firebase() }>
+createRoot(document.getElementById('root')).render(
+  <FirebaseContext.Provider value={new Firebase()}>
     <App />
-  </FirebaseContext.Provider>,
-  document.getElementById('root'),
+  </FirebaseContext.Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
